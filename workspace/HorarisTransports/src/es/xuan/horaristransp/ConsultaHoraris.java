502f8c64a1bf0e1c1141f3af7d7778661141c225
CACAProjectes <cacaprojectsapps@gmail.com>
2021-10-08 13:00:03 +0200

    Nodes i Mapes
    Nodes i Mapes es carreguen a l'hora des de fitxer

--- workspace/HorarisTransports/src/es/xuan/horaristransp/ConsultaHoraris.java
+++ workspace/HorarisTransports/src/es/xuan/horaristransp/ConsultaHoraris.java
@@ -19,31 +19,7 @@ public class ConsultaHoraris {
 		Nodes nodes = carregarNodes2File();
 		System.out.println("Nº de nodes L4: " + nodes.tamany("L4"));
 		System.out.println("Nº de nodes L5: " + nodes.tamany("L5"));
-		//
-		//construirMapa(nodes);
 	}
-	
-	private static void construirMapa(Nodes pNodes) {
-		String strClau = "";
-		int coordX = 0, coordY = 0;
-		// create an Enumeration object to read elements
-        Enumeration<String> e = pNodes.getListaNodes().keys();  
-        // print elements of hashtable using enumeration
-        while (e.hasMoreElements()) {
-        	String auxClau = e.nextElement();
-        	if (!strClau.equals(auxClau) && !strClau.equals("") ) {
-        		coordX = 0;
-        		coordY++;
-        	}
-        	ArrayList<Node> arrNodes = pNodes.getListaNodes().get(auxClau);
-        	for (Node node : arrNodes) {
-        		node.setCoordenada(new Coordenada(coordX++,coordY));
-        		System.out.print(node + " - ");
-        	}
-    		strClau = auxClau;
-    	}		
-	}
-
 	private static Nodes carregarNodes2File() {
 		ArrayList<String> linies = GestioFitxers.llegirFile("c:\\CACAProjectes\\CACAHorarisTransports\\Documentació\\fitxer_importació_horaris.csv");
 		String strClau = "";
@@ -62,12 +38,28 @@ public class ConsultaHoraris {
 	        		coordY++;
 	        	}
 	        	nodeNou.setCoordenada(new Coordenada(coordX++, coordY));
-				nodes.addNode(strValors[0], nodeNou);	// [CLAU, NODE]
+	        	Node nodeMultiple = cercarNode(nodes, nodeNou);
+				nodes.addNode(strValors[0], nodeMultiple);	// [CLAU, NODE]
 	    		strClau = strValors[0];
 	    		//System.out.print(nodeNou + " - ");
 			}
 		}
 		return nodes;
+	}
+
+	private static Node cercarNode(Nodes pNodes, Node pNode) {
+        Enumeration<ArrayList<Node>> e = pNodes.getListaNodes().elements();  
+        // print elements of hashtable using enumeration
+        while (e.hasMoreElements()) {
+        	for(Node node : e.nextElement()) {
+        		if (node.getNom().equalsIgnoreCase(pNode.getNom())) {
+        			String clauNova = pNode.getHoraris().keys().nextElement();
+        			node.getHoraris().put(clauNova, pNode.getHoraris().get(clauNova));
+        			return node;
+        		}
+        	}        	
+        }        	
+		return pNode;
 	}
 
 	private static String horatiToString(String[] pHorari) {
--- workspace/HorarisTransports/src/es/xuan/horaristransp/model/Node.java
+++ workspace/HorarisTransports/src/es/xuan/horaristransp/model/Node.java
@@ -1,21 +1,30 @@
 package es.xuan.horaristransp.model;
 
 import java.io.Serializable;
+import java.util.Hashtable;
+
+import es.xuan.horaristransp.utils.Constants;
 
 public class Node implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String nom;
-	private String horaris;
+	private Hashtable<String, String[]> horaris;
 	private Coordenada coordenada;
 	
 	public Node(String[] strValors) {
 		/*
 		 * L4;Estació RubÍ+D;06:30;06:51;07:13;07:35;07:58;08:22;08:46;09:10;09:34;09:58;10:22;10:46;11:10;11:34;11:58;12:22;12:46;13:10;13:34;13:58;14:22;14:46;15:10;15:34;15:58;16:22;16:46;17:10;17:34;17:58;18:22;18:46;19:10;19:34;19:57;20:19;20:41;21:03;21:25;21:46;22:06
 		 */
+		String[] valors = new String[0];
 		setNom(strValors[1]);			// Nom - Estació RubÍ+D
-		if (strValors.length > 2)		// No tots els nodes tenen horaris
-			setHoraris(strValors[2]);	// Horaris - 06:30;06:51;07:13;07:35;07:58;08:22;08:46;09:10;09:34;09:58;10:22;10:46;11:10;11:34;11:58;12:22;12:46;13:10;13:34;13:58;14:22;14:46;15:10;15:34;15:58;16:22;16:46;17:10;17:34;17:58;18:22;18:46;19:10;19:34;19:57;20:19;20:41;21:03;21:25;21:46;22:06 
+		if (strValors.length > 2) {		// No tots els nodes tenen horaris
+			//setHoraris(strValors[2]);	// Horaris - 06:30;06:51;07:13;07:35;07:58;08:22;08:46;09:10;09:34;09:58;10:22;10:46;11:10;11:34;11:58;12:22;12:46;13:10;13:34;13:58;14:22;14:46;15:10;15:34;15:58;16:22;16:46;17:10;17:34;17:58;18:22;18:46;19:10;19:34;19:57;20:19;20:41;21:03;21:25;21:46;22:06
+			valors = strValors[2].split(Constants.CNT_SEPARADOR_COMA);
+		}
+		setHoraris(new Hashtable<String, String[]>());
+		// L4 - 06:30;06:51;07:13;07:35;07:58,...
+		getHoraris().put(strValors[0], valors);
 		setCoordenada(new Coordenada(0,0));
 	}
 	public String getNom() {
@@ -23,12 +32,6 @@ public class Node implements Serializable {
 	}
 	public void setNom(String nom) {
 		this.nom = nom;
-	}
-	public String getHoraris() {
-		return horaris;
-	}
-	public void setHoraris(String horaris) {
-		this.horaris = horaris;
 	}
 	/**
 	 * @return the coordenada
@@ -46,5 +49,17 @@ public class Node implements Serializable {
 	@Override
 	public String toString() {
 		return getNom() + getCoordenada();
+	}
+	/**
+	 * @return the horaris
+	 */
+	public Hashtable<String, String[]> getHoraris() {
+		return horaris;
+	}
+	/**
+	 * @param horaris the horaris to set
+	 */
+	public void setHoraris(Hashtable<String, String[]> horaris) {
+		this.horaris = horaris;
 	}
 }
